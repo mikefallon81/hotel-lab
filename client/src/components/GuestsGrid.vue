67fcd24a316da7183a90f953f2211e2583cf0659
matmic <madebymat@hotmail.com>
2019-08-14 13:20:57 +0100

    guests grid updated with the components

---
+++ client/src/components/GuestsGrid.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <div id="guestsGrid">
+    <div class="guest" v-for="guest in guests">
+      <h3>Name: {{guest.name}}</h3>
+      <h3>email: {{guest.email}}</h3>
+      <h3>Check in Status {{guest.checkInStatus}}</h3>
+
+      <button>Delete guest</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import GuestService from '@/sevices/GuestService.js'
+
+
+
+export default {
+  name: "guests-grid",
+  props: ["guests"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
