1657fe734fc6d0ccc314f60c7dfb4dc2378a73f4
matmic <madebymat@hotmail.com>
2019-08-14 15:29:02 +0100

    css styling added

--- client/src/components/GuestsGrid.vue
+++ client/src/components/GuestsGrid.vue
@@ -1,13 +1,18 @@
 <template lang="html">
+  <div>
+
+    <h1>Guests</h1>
   <div id="guestsGrid">
     <div class="guest" v-for="guest in guests">
       <h3>Name: {{guest.name}}</h3>
       <h3>email: {{guest.email}}</h3>
-      <h3>Check in Status: {{guest.checkInStatus}}</h3>
+      <h3 v-if="guest.checkInStatus === true">Checked in</h3>
+      <h3 v-else>Checked out</h3>
 
-      <button v-on:click="handleDelete(guest._id)">Delete guest</button>
+      <button class="deleteBtn" v-on:click="handleDelete(guest._id)">Delete guest</button>
     </div>
   </div>
+</div>
 </template>
 
 <script>
@@ -29,4 +34,22 @@ methods: {
 </script>
 
 <style lang="css" scoped>
+#guestsGrid {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+}
+
+  .guest {
+    border-style:double;
+    margin: 5px;
+    padding: 10px;
+  }
+
+  .deleteBtn {
+    font-weight: bold;
+    color: red;
+    border-width: thick;
+    margin-bottom: 5px;
+  }
 </style>
