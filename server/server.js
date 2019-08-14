861b668774ce4ade6fd2498bd3f026aa4b391637
matmic <madebymat@hotmail.com>
2019-08-14 11:39:23 +0100

    updated body-parser link

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const parser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
